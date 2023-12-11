64b645e66243656284fc2d0a061d3c4bf79a2620
Drew Laikin <laikinde@miamioh.edu>
2023-12-11 00:16:31 -0500

    minor rework

--- EmbraceAI/JS/main.js
+++ EmbraceAI/JS/main.js
@@ -1,11 +1,8 @@
+const HaddadBio = "Charlie leads the Enterprise Business Architecture for Lilly global Tech team. His role is to help our Tech organization deliver digital solutions faster and with higher value that drive Lilly's company growth and success. He leads a talented team of innovators and architects who create and shape solutions for our strategic business priorities, such as improving customer experience, accelerating research and development, enhancing data analytics, and increasing operational efficiency. He also partners closely with our Digital Core teams to ensure that our enterprise foundation tech stack is modern, effective, and scalable. During his more than 30 years at Lilly, Charlie has been a software leader focused on delivering digital innovations in pharmaceutical drug development across many therapeutic areas. Most recently, he was responsible for enabling excellent patient experiences through clinically relevant, digital health software and devices. Prior to that, the majority of roles were in Medicines Development IT where he led the harmonization, integration and automation of clinical information management and data flow. He also had senior leadership roles in our global infrastructure organization enabling digital platform solutions and employee enablement across our enterprise. Charlie earned his bachelor's degree in Computer Systems Analysis from Miami University. He lives in Indianapolis with his wife and they have three grown children spread across the country.";
 
 $(document).ready(function() {
   // Requiring fs module in which 
   // readFile function is defined.
-  fs.readFile('textTest.txt', (err, data) => {
-    if (err) throw err;
-    console.log('beans', data.toString());
-  })
 
   // Add a shadow effect to Bootstrap cards on hover
   $('.hoverOn').hover(function() {
