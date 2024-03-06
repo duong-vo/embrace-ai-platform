ae7e4b94cbc5a18b5f0eac855442489387ef34f9
Drew Laikin <dlaikin3@gmil.com>
2024-03-06 14:39:07 -0500

    whatever

--- EmbraceAI/JS/resources.js
+++ EmbraceAI/JS/resources.js
@@ -53,8 +53,16 @@ function pushCard(obj) {
     const { type, itemNum, title, source, link, embed, imgLink, tag, description } = obj;
     if (!$("."+type).length) {
         const inum = "item-" + itemNum.split("-")[1];
-        $("#putHere").append(`<a class="sidenavlink btn-primary nav-link ${type}" data-bs-toggle="collapse" role="button" aria-expanded="false" href="#${type}">${type === "opensource" ? "Open Source": type.charAt(0).toUpperCase() + type.slice(1) + "s"}</a>`);
-        $("#putHere").append($("#putHere").length > 1 ? `</div><div id="${type} class="collapse">` : `<div id="${type}" class="collapse">`);
+        $("#putHere").append(`
+        <a class="sidenavlink btn-primary nav-link ${type}" data-bs-toggle="collapse" role="button" 
+        aria-expanded="false" href="#${type}" onclick="expCheck("${inum}")"id="colLink${inum}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16" id="svg${inum}">
+        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+        </svg>
+        ${type === "opensource" ? "Open Source": 
+        type.charAt(0).toUpperCase() + type.slice(1) + "s"}</a>`);
+        $("#putHere").append($("#putHere").length > 1 ? 
+        `</div><div id="${type} class="collapse">` : `<div id="${type}" class="collapse">`);
         $(".resourcesHere").append(`<br id="${inum}">`);
     }
     $(`#${type}`).append(`<a class="nav-link ms-3 my-1 ${type}" href="#${itemNum}">${title}</a>`);
@@ -62,7 +70,7 @@ function pushCard(obj) {
     `
         <br>
         <div class="card rcard h-100 ${tag.replace(" ", "")}" id="${itemNum}">
-        ${imgLink == "no"? ``: `<img src="${imgLink}" class="card-img-top" alt=""></img>`}
+        ${imgLink == "no"? ``: `<img src="${imgLink}" class="${type==="Book" ? "img-fluid rounded-start" : "card-img-top"}" alt="Cover image"></img>`}
         <div class="card-body">
             <h5 class="card-title">${title}</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">${source}</h6>
@@ -71,6 +79,23 @@ function pushCard(obj) {
         </div>
         </div>
         <br>
+    `;
+    const bookCard = `
+    <div class="card rcard h-100 ${tag.replace(" ", "")}" id="${itemNum}">
+        <div class="row g-0">
+            <div class="col-md-4">
+            ${imgLink == "no"? ``: `<img src="${imgLink}" class="${type==="Book" ? "img-fluid rounded-start" : "card-img-top"}" alt="Cover image"></img>`}
+            </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${source}</h6>
+                        <p class="card-text">${description}</p>
+                        <a href="${link}" class="card-link">Go to Resource</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     `;
     const videoCard =
     `
@@ -88,8 +113,9 @@ function pushCard(obj) {
         </div>
         <br>
     `;
-    if (["article", "report", "Book", "course", "opensource"].includes(type)) $(".resourcesHere").append(defaultCard);
+    if (["article", "report", "course", "opensource"].includes(type)) $(".resourcesHere").append(defaultCard);
     else if (type == "video") $(".resourcesHere").append(videoCard);
+    else if (type == "Book") $(".resourcesHere").append(bookCard);
 }
 
 function advancedRec(obj) {
@@ -98,3 +124,13 @@ function advancedRec(obj) {
     for (let element in obj) for (let each in search) points += obj[element].toLowerCase().includes(each) ? each.length : 0;
     return points/$("#searchBar").val().length > 0.6;
 }
+
+function expCheck(inum) {
+    const newPath = $("#colLink"+inum).attr('aria-expanded') === 'true' ?
+        `<path fill-rule="evenodd" d="M3.646 11.854a.5.5 0 0 0 .708 0L8 8.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708M2.4 5.2c0 .22.18.4.4.4h10.4a.4.4 0 0 0 0-.8H2.8a.4.4 0 0 0-.4.4"/>`
+        : `<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>`;
+    $("#svg" + inum).empty();
+    $("#svg" + inum).append(newPath);
+    
+}
+  
