af77a01d7f5a3b65ee2768321cbdc0b04957c8da
duong-vo <vodh2@miamioh.edu>
2024-04-22 18:50:43 -0200

    update conference, waiting on video link

--- EmbraceAI/JS/conf.js
+++ EmbraceAI/JS/conf.js
@@ -2,6 +2,7 @@
 // DOCUMENT READY
 $(document).ready(function() {
     initCountdown();
+    addImages();
 });
 
 
@@ -20,10 +21,26 @@ function initCountdown() {
       const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-  
+      if (days < 0) {
       // Update the countdown display
-      var pill = `<span class="badge text-bg-secondary"><h3>${days} Days Until Embrace AI Conference<h3></span>`;
+        var pill = `<span class="badge text-bg-secondary"><h3>The conference has ended!<h3></span>`;
+      }
       $("#timer").empty();
       $("#timer").append(pill);
     }, 1000);
 }
+
+function addImages() {
+  for (let i = 1; i <= 18; i++) {
+    $("#gallery").append(`
+      <div class="col-lg-4 align-items-center" style="margin-bottom: 10px">
+        <img src="imgs/conf/${i}.jpeg" class="img-fluid" style="max-height: 100%; max-width: 100%; object-fit: cover;">
+      </div>
+    `)
+  }
+}
+
+// function hmagesList() {
+//   const path = '
+//   return ['','']
+// }
